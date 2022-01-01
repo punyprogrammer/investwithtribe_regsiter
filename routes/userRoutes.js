4bb4fef9abe9957e50f5043fc0db4752d003f703
Amardeep Ganguly <you@example.com>
2022-01-01 19:41:54 +0530

    fixed-get-routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -41,7 +41,7 @@ router.post("/email", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    const {password,...others}=user
+    const {password,...others}=user._doc;
     res.status(200).json(others);
   } catch (error) {
     res.status(500).json(error);
